1e720cf0a6495a5dc77024c409d2c90d4ec32e98
Jason Stewart <jason00stewart@gmail.com>
2025-02-14 13:33:46 -0700

    v2v works with refinement

--- tests/tstMaps.cpp
+++ tests/tstMaps.cpp
@@ -93,5 +93,36 @@ TYPED_TEST(MapsTest, test_v2v_refinement0)
     this->test_v2v(0);
 }
 
+/**
+ * Tests that the v2v map is built correctly one two iterations
+ * of uniform refinement
+ */
+TYPED_TEST(MapsTest, test_v2v_refinement2)
+{
+    int mesh_size = this->comm_size_ * 2;
+    if (this->comm_size_ == 1)
+    {
+        mesh_size = 5;
+    }
+    
+    this->init(mesh_size, 1);
+
+    auto vef_gid_start = this->mesh_->vef_gid_start();
+
+    // Uniform refinement
+    for (int i = 0; i < 2; i++)
+    {
+        int num_local_faces = this->mesh_->count(NuMesh::Own(), NuMesh::Face());
+        int face_gid_start = vef_gid_start(this->rank_, 2);
+        Kokkos::View<int*, Kokkos::HostSpace> fin("fin", num_local_faces);
+        for (int i = 0; i < num_local_faces; i++)
+        {
+            fin(i) = face_gid_start + i;
+        }
+        this->performRefinement(fin);
+    }
+    this->test_v2v(0);
+}
+
 
 } // end namespace NuMeshTest
